141bc8def9aa54404390b84886b68c6ba3030233
Jamal_DH <Solay.abris@gmail.com>
2025-10-20 22:16:28 +0300

    u

---
+++ p.py
@@ -0,0 +1,15 @@
+# for i in range (12,260,10):
+#     print(i)
+# c = 12 
+# while c < 260 :
+#      print (c)
+#      c = c+10
+
+
+# c = 45 
+# while c>=3:
+#         print (c*2)
+#         c = c-5
+
+# for num in range (45,2,-5):
+#     print (num*2)
